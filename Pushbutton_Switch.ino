c4b6ee5def3e60dd3ee7ccffb232b503cb7cff64
lawtalker <lawtalker@users.noreply.github.com>
2022-03-15 10:58:42 -0700

    Update Pushbutton_Switch.ino
    fix typo in comment

--- Pushbutton_Switch.ino
+++ Pushbutton_Switch.ino
@@ -24,7 +24,7 @@
    A momentary-on pushbutton is debounced in software.  Every button press
    toggles the state of a controlled load.
 
-   The load is controlled using a 5v relay board using a LOW trigge and the 
+   The load is controlled using a 5v relay board using a LOW trigger and the 
    NC terminals.  This way, when the relay board is not powered, the load is 
    connected.  When the relay board is powered, then a LOW signal triggers 
    the relay, and the load is then disconnected.  The NC terminals are wired 
